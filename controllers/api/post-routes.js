880123f1fbed1adbe7f9f652276d1d28e90fbbb4
unknown <peterlim995@gmail.com>
2023-01-18 11:28:33 -0500

    cloudlary use

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,24 +2,85 @@ const router = require('express').Router();
 const { Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 const { format_date } = require('../../utils/helpers');
+// for cloudinary use
+require('dotenv').config();
+const cloudinary = require("cloudinary");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/"});
+// const upload = multer();
 
 router.get('/', (req, res) => {
     console.log('GET request received!');
 });
 
 // Create Post in the back end after user submits on the front end
-router.post('/', async (req, res) => {
+router.post('/', withAuth, upload.single('file'), async (req, res) => {
   try {
+    console.log('body: ',req.body);
+    console.log('file: ',req.file) ;
+
+    const file = req.file.path;
+    console.log('file path: ', file);
+    const result = await cloudinary.uploader.upload(file, {
+      resource_type: 'auto',
+      folder: 'community-chat'
+    });
+
+    console.log('result: ', result);
+
+    const media_id = result.public_id;
+
     const newPostData = await Post.create({
         post_title: req.body.title,
         post_content: req.body.content,
+        media: media_id,
         user_id: req.session.user_id
     })
-    res.json(newPostData);
+    res.status(200).json(newPostData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+// Create profile
+router.put('/profile', withAuth, upload.single('file'), async (req, res) => {
+  try {
+    console.log('body: ',req.body);
+    console.log('file: ',req.file) ;
+
+    const file = req.file.path;
+    console.log('file path: ', file);
+    const result = await cloudinary.uploader.upload(file, {
+      resource_type: 'auto',
+      folder: 'community-chat/profile'
+    });
+
+    console.log('result: ', result);
+
+    const profile_picture = result.public_id;
+
+    const newUserData = await User.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        date_of_birth: req.body.date_of_birth,
+        profile_picture: profile_picture,
+      },
+      {
+        where: {
+          id: req.session.user_id
+        }
+      }
+    )
+    console.log("newUserData: ",newUserData);
+    res.status(200).json(newUserData);    
+    // res.status(200).json('');  
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 
 // Delete a single post by id
 router.delete('/:id', withAuth, async (req, res) => {
@@ -35,7 +96,7 @@ router.delete('/:id', withAuth, async (req, res) => {
 });
 
 // finds the post the user requested to edit and displays it in an editable format
-router.get('/edit-post/:id', async (req, res) => {
+router.get('/edit-post/:id', withAuth, async (req, res) => {
   console.log("Post ID:" + req.params.id);
   post_id = req.params.id;
   try {
