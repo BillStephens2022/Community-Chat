b24d50f571e3dde105442e845e8b2c919ed7ccaf
unknown <peterlim995@gmail.com>
2023-01-21 16:30:29 -0500

    again

--- public/js/showImages.js
+++ public/js/showImages.js
@@ -1,3 +1,45 @@
+
+  const buttonHandler = async (event) => {
+    // console.log('hit the button');
+    // console.log('Event', event);
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+      const btnFunction = event.target.getAttribute('data-function');
+      console.log('data-id: ',id);
+      console.log('data-function: ',btnFunction);
+  
+      if (btnFunction === 'delete') {
+        const delOk = confirm("Do you want to delete the blog?");
+        if (delOk) {
+          const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+          });
+  
+          if (response.ok) {
+            document.location.replace('/dashboard');
+          } else {
+            alert('Failed to delete blog');
+          }
+        }
+      } else if (btnFunction === 'edit') {        
+            document.location.replace(`/api/posts/edit-post/${id}`);      
+      }
+    }
+  }
+  
+  
+  document.querySelector('.post-list').addEventListener('click', buttonHandler);
+
+
+
+
+
+
+
+
+
+
+
 function call(event) {
     console.log('event: ', event);
     console.log('event target: ', event.target);
