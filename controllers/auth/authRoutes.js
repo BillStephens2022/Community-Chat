56e13a392c60d92d5e91c8b38fc72499947f7842
BillStephens2022 <two4onebill@yahoo.com>
2023-01-23 07:42:36 -0500

    working on error handling for login

--- controllers/auth/authRoutes.js
+++ controllers/auth/authRoutes.js
@@ -1,9 +1,38 @@
 const router = require('express').Router();
 const passport = require('passport');
 const { logRouteInfo } = require('../../utils/auth');
+// const csrf = require('csurf');
 
-console.log(logRouteInfo);
-// Endpoint /auth
+// router.locals.pluralize = require('pluralize');
+
+// router.use(csrf());
+
+// router.use(function(req, res, next) {
+//   var msgs = req.session.messages || [];
+//   res.locals.messages = msgs;
+//   res.locals.hasMessages = !! msgs.length;
+//   req.session.messages = [];
+//   console.log(req.session.messages);
+//   next();
+// });
+
+// router.use(function(req, res, next) {
+//   res.locals.csrfToken = req.csrfToken();
+//   next();
+// });
+
+// router.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+
+// // error handler
+// router.use(function(err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('login');
+// })
 
 router.post('/register',
     logRouteInfo,
@@ -19,8 +48,12 @@ router.post(
       successRedirect: "/dashboard",
       failureRedirect: "/login",      
       failureMessage: true
-    }),
-    passport.authenticate('local', { failureFlash: 'Invalid username or password.' })
+    }, (err, user, options) => {
+      let failureMsg = options.message;
+      console.log(failureMsg);
+    })
   );
+
+  
 
 module.exports = router;
