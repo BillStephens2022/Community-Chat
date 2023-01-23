d5e31d69cf56fa07197bb5d34758e2dbb2b6a033
BillStephens2022 <two4onebill@yahoo.com>
2023-01-23 07:47:30 -0500

    added back failureRedirect on register route

--- controllers/auth/authRoutes.js
+++ controllers/auth/authRoutes.js
@@ -1,43 +1,12 @@
 const router = require('express').Router();
 const passport = require('passport');
 const { logRouteInfo } = require('../../utils/auth');
-// const csrf = require('csurf');
-
-// router.locals.pluralize = require('pluralize');
-
-// router.use(csrf());
-
-// router.use(function(req, res, next) {
-//   var msgs = req.session.messages || [];
-//   res.locals.messages = msgs;
-//   res.locals.hasMessages = !! msgs.length;
-//   req.session.messages = [];
-//   console.log(req.session.messages);
-//   next();
-// });
-
-// router.use(function(req, res, next) {
-//   res.locals.csrfToken = req.csrfToken();
-//   next();
-// });
-
-// router.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-
-// // error handler
-// router.use(function(err, req, res, next) {
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   res.status(err.status || 500);
-//   res.render('login');
-// })
 
 router.post('/register',
     logRouteInfo,
     passport.authenticate("local-signup", {
-        successRedirect: "/profile",        
+        successRedirect: "/profile",
+        failureRedirect: "/login",       
     })
 );
 
